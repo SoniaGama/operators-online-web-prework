547915d239d5461d6b7d17ef9f044e601ebfd093
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-18 05:53:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
-  if speed
+  if speed < 40 && speed > 60
+    true
   else
+    false
   end
 
 end
 
-
+Debería volver true si la velocidad es inferior 40 o superior 60
 
 def not_safe?(speed)
 	
